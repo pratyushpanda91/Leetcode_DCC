20a3dc1b5f5a7adbf3cd26de06adcf7e18bdd098
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2024-09-05 23:27:47 +0530

    Day 37 (05/09)

--- 36_Missing_in_Array.cpp
+++ 36_Missing_in_Array.cpp
@@ -1,6 +1,27 @@
 class Solution {
-  public:
-    int missingNumber(int n, vector<int>& arr) {
-     return ((n * (n+1)) / 2) - accumulate(arr.begin(), arr.end(), 0);
-}
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int nNumsSum = 0;
+        for(int i : rolls) nNumsSum += i;
+
+        int m=rolls.size();
+        int totalLength = n+m;
+
+        int mNumsSum = mean*totalLength - nNumsSum;     
+                                                        
+
+        if(n*6 < mNumsSum || mNumsSum<n) return {};     
+        
+        int rem = mNumsSum % n;
+        int q   = mNumsSum / n;
+
+        vector<int> res(n,q);
+        for(int i=1; i<=rem; i++)
+            res[i]++;
+
+        return res;
+    }
 };
