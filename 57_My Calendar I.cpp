fc0a3452b94cd041430bad74b063b66940fa69be
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2024-09-26 17:59:28 +0530

    57 26/09

---
+++ 57_My Calendar I.cpp
@@ -0,0 +1,23 @@
+struct Node{
+    int s, t;
+    Node *left, * right;
+    Node(int s, int t): s(s), t(t), left(NULL), right(NULL){}
+};
+class MyCalendar {
+    Node* root;
+public:
+    MyCalendar(): root(NULL) {}
+    
+    bool dfs(int s, int t, Node* &curr){
+        if (!curr) {
+            curr=new Node(s, t);
+            return 1;
+        }
+        if (t<=curr->s) return dfs(s, t, curr->left);
+        else if (s>=curr->t) return dfs(s, t, curr->right);
+        else return 0;
+    }
+    bool book(int start, int end) {
+        return dfs(start, end, root);
+    }
+};
