06dd4edcb3d4e669a79bf0671050784b7e73c086
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-02-06 12:43:39 +0530

    096/02

---
+++ 2025/2.February/6_1726. Tuple with Same Product.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static int tupleSameProduct(vector<int>& nums) {
+        const int n=nums.size(), N=n*(n-1)/2;
+        vector<int> product;
+        product.reserve(N);
+        for(int i=0; i<n-1; i++)
+            for(int j=i+1; j<n; j++)
+                product.push_back(nums[i]*nums[j]);
+        sort(product.begin(), product.end());
+        int ans=0, j=0, f=1;
+        for(int i=1; i<N; i++){
+            if (product[j]==product[i]) f++;
+            else{
+                if (f>1) ans+=f*(f-1)*4;
+                f=1;
+                j=i;
+            }
+        } 
+        return ans+=(j==N-1)?f*(f-1)*4:0;
+    }
+};
