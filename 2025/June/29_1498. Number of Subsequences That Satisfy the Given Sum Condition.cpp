4000f9b611cefa98cf7b9981de16f5000b4b64a7
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-06-29 10:53:47 +0530

    Create 29_1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp

---
+++ 2025/June/29_1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        const int mod = 1e9 + 7;
+        int n = nums.size();
+        int f[n + 1];
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            f[i] = (f[i - 1] * 2) % mod;
+        }
+        int ans = 0;
+        for (int i = 0; i < n && nums[i] * 2 <= target; ++i) {
+            int j = upper_bound(nums.begin() + i + 1, nums.end(), target - nums[i]) - nums.begin() - 1;
+            ans = (ans + f[j - i]) % mod;
+        }
+        return ans;
+    }
+};
