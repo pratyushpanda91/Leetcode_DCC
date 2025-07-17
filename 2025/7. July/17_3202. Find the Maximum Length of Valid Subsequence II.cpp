29b771d18020a82705f47e332131a0162f84eae0
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-07-17 19:41:38 +0530

    Create 17_3202. Find the Maximum Length of Valid Subsequence II.cpp

---
+++ 2025/7. July/17_3202. Find the Maximum Length of Valid Subsequence II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static int maximumLength(vector<int>& nums, int k) {
+        short ans=0;
+        vector<short> dp;
+        for (int i=0; i<k; i++){
+            dp.assign(k, 0);
+            for (int x : nums){
+                int j=x%k;
+                dp[j]=max(dp[j], short(dp[(i+k-j)%k]+1));
+                ans=max(ans, dp[j]);
+            }
+        }
+        return ans;
+        
+    }
+};
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
