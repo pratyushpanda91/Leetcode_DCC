7f1b4a35ff7799259fcae960a618743eaf322087
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-08-14 20:10:54 +0530

    Create 14_2264. Largest 3-Same-Digit Number in String.cpp

---
+++ 2025/8.August/14_2264. Largest 3-Same-Digit Number in String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string largestGoodInteger(string& num) {
+        int count=0;
+        char prev='X', maxd=' ';
+        for(char c: num){
+            if (c==prev) count++;
+            else count=1;
+            if (count==3)
+                maxd=max(maxd, c);
+            prev=c;
+        }
+        return (maxd==' ')?"":string(3, maxd);
+    }
+};
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
