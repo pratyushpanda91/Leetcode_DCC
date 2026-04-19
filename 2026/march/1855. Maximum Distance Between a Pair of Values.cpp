3dca650693e6802ea0ccad1710d65c55d4f29298
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2026-04-19 21:10:33 +0530

    Add solution for maximum distance between values

---
+++ 2026/march/1855. Maximum Distance Between a Pair of Values.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0;
+        int n = nums1.size(), m = nums2.size();
+        int ans = 0;
+
+        while (i < n && j < m) {
+            if (nums1[i] <= nums2[j]) {
+                ans = max(ans, j - i);
+                j++;
+            } else {
+                i++;
+                if (i > j) j = i;
+            }
+        }
+
+        return ans;
+    }
+};
